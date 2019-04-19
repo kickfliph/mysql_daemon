167bbb75c5ad6b71f806ad02f09e42e8cd8916bb
Harold G Luzardo B <haroldburtonl@gmail.com>
2019-04-19 01:28:18 -0400

    Update mysqld.sh

--- mysqld.sh
+++ mysqld.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 EMAIL="luisedgardoortiz@gmail.com"
-SERVICE='mysqld'
-NAME='hostname'
-STATUS='pgrep $SERVICE | wc -l'
-if [ $STATUS -ne 1 ]
+NAME=`hostname`
+STATUS=`pgrep mysql | wc -l`
+echo "$STATUS"
+
+if [ $STATUS -ne 2 ];
 then
-    ps -ef | grep $SERVICE | grep -v grep | awk '{print $2}' | xargs kill
-    service $SERVICE start
-    echo "Mysql Server Restarted at: `date`" |mail -s "Alert ($NAME) : Mysql Service is not running (Manually Restarted)" $EMAIL
+    service mysql start
+    echo "Mysql Server Restarted at: `date`" | mail -s "Alert ($NAME) : Mysql Service is not running (Manually Restarted)" $EMAIL
 fi
